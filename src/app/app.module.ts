cf4e98789d65135b0ed19429d5a4ba1e99be6172
Nikolay Nikolaev <nikolaev.a.nikolay@gmail.com>
2017-06-05 23:59:21 +0300

    Add scss support + add shared module + add custom material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { SharedModule } from './shared/shared.module';
+import { CustomMaterialModule } from './custom-material/custom-material.module';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +15,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    CustomMaterialModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
